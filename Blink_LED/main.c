6be5ece608306e6227a191b71b437edf463e1b9b
Peter Pan <142995759+PeterPan4102@users.noreply.github.com>
2025-10-12 11:48:20 +0700

    Update main.c

--- Blink_LED/main.c
+++ Blink_LED/main.c
@@ -1,24 +1,25 @@
 #include "S32K144.h"
+#include "device_registers.h"            /* include peripheral declarations S32K144 */
 
 void delay_ms(uint32_t ms);
 
 int main(void)
 {
     /* Bật clock cho PORTD */
-    PCC->PCCn[PCC_PORTD_INDEX] |= PCC_PCCn_CGC_MASK;
+    IP_PCC->PCCn[PCC_PORTD_INDEX] |= PCC_PCCn_CGC_MASK;
 
     /* Cấu hình chân PTD0 làm GPIO output */
-    PTD->PDDR |= (1 << 0);          // output
-    PORTD->PCR[0] = PORT_PCR_MUX(1); // MUX = 1 (GPIO)
+    IP_PTD->PDDR |= (1 << 0);          // output
+    IP_PORTD->PCR[0] = PORT_PCR_MUX(1); // MUX = 1 (GPIO)
 
     while (1)
     {
         /* Bật LED */
-        PTD->PCOR = (1 << 0);   // ghi 0 để bật LED 
+        IP_PTD->PCOR = (1 << 0);   // ghi 0 để bật LED (tùy board, có thể ngược)
         delay_ms(3000);         // delay 3 giây
 
         /* Tắt LED */
-        PTD->PSOR = (1 << 0);
+        IP_PTD->PSOR = (1 << 0);
         delay_ms(3000);
     }
 }
@@ -27,6 +28,6 @@ void delay_ms(uint32_t ms)
 {
     uint32_t i;
     for (i = 0; i < ms * 4000; i++) {
-        __NOP();
+        NOP();
     }
 }
