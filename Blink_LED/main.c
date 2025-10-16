c86226d16a7bc761ab7005ed2da299d0f48d8e7c
Peter Pan <142995759+PeterPan4102@users.noreply.github.com>
2025-10-16 23:00:51 +0700

    Add control for multiple LEDs in main.c

--- Blink_LED/main.c
+++ Blink_LED/main.c
@@ -11,14 +11,34 @@ int main(void)
     /* Cấu hình chân PTD0 làm GPIO output */
     IP_PTD->PDDR |= (1 << 0);          // output
     IP_PORTD->PCR[0] = PORT_PCR_MUX(1); // MUX = 1 (GPIO)
+    IP_PTD->PDDR |= (1 << 15);          // output
+    IP_PORTD->PCR[15] = PORT_PCR_MUX(1); // MUX = 1 (GPI15)
+    IP_PTD->PDDR |= (1 << 16);          // output
+    IP_PORTD->PCR[16] = PORT_PCR_MUX(1); // MUX = 1 (GPI16)
 
     while (1)
     {
-        /* Bật LED */
-        IP_PTD->PCOR = (1 << 0);   // ghi 0 để bật LED (tùy board, có thể ngược)
+        /* Bật LED RED*/
+        IP_PTD->PCOR = (1 << 15);   // ghi 0 để bật LED 
         delay_ms(3000);         // delay 3 giây
 
-        /* Tắt LED */
+        /* Tắt LED RED*/
+        IP_PTD->PSOR = (1 << 15);
+        delay_ms(3000);
+        
+        /* Bật LED GREEN*/
+        IP_PTD->PCOR = (1 << 16);   // ghi 0 để bật LED 
+        delay_ms(3000);         // delay 3 giây
+
+        /* Tắt LED GREEN*/
+        IP_PTD->PSOR = (1 << 16);
+        delay_ms(3000);
+        
+        /* Bật LED BLUE*/
+        IP_PTD->PCOR = (1 << 0);   // ghi 0 để bật LED 
+        delay_ms(3000);         // delay 3 giây
+
+        /* Tắt LED BLUE*/
         IP_PTD->PSOR = (1 << 0);
         delay_ms(3000);
     }
